c23ffbe7257aa602816cab30c1ced503bbbe79ee
lota <christiansolomon592@gmail.com>
2023-10-22 19:22:32 +0100

    testing functionalities

--- cypress/e2e/test/jsAlert.spec.cy.js
+++ cypress/e2e/test/jsAlert.spec.cy.js
@@ -9,4 +9,32 @@ describe('Alerts', ()=>{
 
         cy.get('#result').should('have.text', 'You successfully clicked an alert')
     })
+
+    it('js confirm alert', function(){
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+
+        cy.get("button[onclick='jsAlert()']").click() 
+        cy.on('window:confirm', (t)=>{ 
+            expect(t).to.contains('I am a js Confirm')
+        })
+
+        //cypress automatically close the alert window by using ok button(default)
+
+        cy.get('#result').should('have.text', 'You clicked: Ok')
+
+    })
+
+    it('js confirm alert - cancel ', function(){
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+        
+        cy.get("button[onclick='jsAlert()']").click() 
+        cy.on('window:confirm', (t)=>{ 
+            expect(t).to.contains('I am a js Confirm')
+            })
+
+        cy.on('window:confirm', ()=> false) // cypress closes alert window using cancel button    
+        
+        cy.get('#result').should('have.text', 'You clicked: Cancel')
+        
+    })
 })
