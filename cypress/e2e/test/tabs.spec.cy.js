bf17bb8c46741327b455ce23890c7cda747e8c2e
lota <christiansolomon592@gmail.com>
2023-10-24 06:55:53 +0100

    testing functionalities

--- cypress/e2e/test/tabs.spec.cy.js
+++ cypress/e2e/test/tabs.spec.cy.js
@@ -2,5 +2,16 @@ describe('Handling tabs', function(){
     it('Approach 1', function(){
         cy.visit('https://the-internet.herokuapp.com/windows')
         cy.get('.example >a').invoke('removeAttr', 'target').click()
+
+        cy.url().should('include', 'https://the-internet.herokuapp.com/windows/new')
+
+        cy.go('back') //back to parent tab
+    })
+
+    it('Approach 2', function(){
+        cy.visit('https://the-internet.herokuapp.com/windows')
+        cy.get('.example >a').invoke('removeAttr', 'target').click()
+
+
     })
 })
