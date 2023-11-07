d0c29eadce716512efe625934e9a422f8b9f7e48
lota <christiansolomon592@gmail.com>
2023-11-07 13:49:08 +0100

    testing functionalities

--- cypress/e2e/test/fileupload.spec.cy.js
+++ cypress/e2e/test/fileupload.spec.cy.js
@@ -5,6 +5,7 @@ describe('File upload', ()=>{
         cy.visit('https://the-internet.herokuapp.com/upload')
         cy.get('#file-upload').attachFile('testdata.xlsx')
 
+
         cy.get('#file-submit').click()
 
         // cy.get('.large-4 > div').should("include", 'Powered')
@@ -12,5 +13,12 @@ describe('File upload', ()=>{
 
     it.only('Upload file - Rename', function (){
         cy.visit('https://the-internet.herokuapp.com/upload')
+
+        cy.get('#file-upload').attachFile({filePath:'testdata.xlsx', fileName:'exceldata.xlsx'})
+        cy.get('#file-submit').click()
+
+        cy.wait(3000)
+
+        cy.get('h3').should('have.text', 'File Uploaded!')
     })
 })
