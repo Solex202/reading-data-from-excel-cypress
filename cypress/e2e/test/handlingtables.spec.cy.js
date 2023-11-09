4236fd52134efa85f74fa94539559dca1b003d3b
lota <christiansolomon592@gmail.com>
2023-11-09 10:52:35 +0100

    cypress custom commands

--- cypress/e2e/test/handlingtables.spec.cy.js
+++ cypress/e2e/test/handlingtables.spec.cy.js
@@ -8,9 +8,16 @@ describe('Handling tables', function (){
     })
     it('Check cell data from specific row & column', function (){
 
+
     })
     it('Read all the rows and columns data in the first page', function (){
-
+        cy.get('').each(($row, index, $rows)=>{
+            cy.wrap($row).within(()=>{
+                cy.get('td' ).each(($col, index, $cols)=>{
+                    cy.log($col.text())
+                })
+            })
+        })
     })
     it('Pagination', function (){
 
