6f9057e2c50814d58bbc45fdfdd78ee805529e9d
lota <christiansolomon592@gmail.com>
2023-11-09 09:50:54 +0100

    cypress custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,6 +4,7 @@ require('cypress-iframe');
 require('@4tw/cypress-drag-drop')
 const{MailSlurp} = require('mailslurp-client')
 
+
 const mailslurp = new MailSlurp({ apiKey: Cypress.env('EMAIL_API_KEY')});
 
 
@@ -23,3 +24,42 @@ Cypress.Commands.add('parseXlsx', (inputFile)=>
 Cypress.Commands.add('clickLink', (label)=>{
     cy.get('a').contains(label).click()
 })
+
+// Cypress.Commands.overwriteQuery('contains', (originalFn.bind(this), subject, filter, text, options = {})=>{
+//     if (typeof text == 'object'){
+//         options = text
+//         text = filter
+//         filter = undefined
+//     }
+//
+//     options.matchCase = false
+//
+//     return originalFn(subject, filter, text, options)
+// })
+
+Cypress.Commands.overwriteQuery(
+    "contains",
+    function (contains, filter, text, userOptions = {}) {
+
+        // This is parameter resolution from Cypress v12.7.0 source
+        if (Cypress._.isRegExp(text)) {
+            // .contains(filter, text)
+            // Do nothing
+        } else if (Cypress._.isObject(text)) {
+            // .contains(text, userOptions)
+            userOptions = text
+            text = filter
+            filter = ''
+        } else if (Cypress._.isUndefined(text)) {
+            // .contains(text)
+            text = filter
+            filter = ''
+        }
+
+        userOptions.matchCase = false;
+
+        let contains0 = contains.bind(this)    // this line fixes the error
+
+        return contains0(filter, text, userOptions)
+    }
+)
